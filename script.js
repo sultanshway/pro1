feeea313f358c2bd7b2623f1a79dee6307c7876b
sultanshway <sultanshway@gmail.com>
2026-01-20 22:30:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("hello, Coder Shiyar");
+
+let codes = document.getElementById("codes");
+let play = document.getElementById("play");
+let remove = document.getElementById("remove");
+let result = document.getElementById("result");
+
+play.addEventListener('click', ()=>{
+    result.innerHTML = codes.value;
+    localStorage.setItem("x", codes.value)
+});
+
+remove.addEventListener('click', ()=>{
+    result.innerHTML = "";
+    codes.value = "";
+});
+
+onload = ()=>{
+    codes.value = localStorage.getItem("x");
+    result.innerHTML = codes.value;
+};
